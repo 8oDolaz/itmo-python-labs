ed67f103d505730bf28f0fea76cba337775d7dcf
8oDolaz <gzagranichnov@mail.ru>
2023-09-18 17:18:04 +0300

    Commented text removed

--- lab_2/french_flag.py
+++ lab_2/french_flag.py
@@ -3,10 +3,6 @@ from config import RED, BLUE, WHITE, END
 def draw_flag() -> None:
     strip_length, flag_length = 7, 5
 
-    # flag = []
-    # for _ in range(flag_length):
-    #     flag.append(f'{BLUE}{" " * strip_length}{WHITE}{" " * strip_length}{RED}{" " * strip_length}{END}')
-
     print('\n'.join(
         f'{BLUE}{" " * strip_length}{WHITE}{" " * strip_length}{RED}{" " * strip_length}{END}'
         for _ in range(flag_length)
