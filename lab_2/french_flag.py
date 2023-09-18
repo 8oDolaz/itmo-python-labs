0661d6b1d4dde624a4dbc21fb06385bf795d4649
8oDolaz <gzagranichnov@mail.ru>
2023-09-18 17:16:48 +0300

    String instead of list flag task

--- lab_2/french_flag.py
+++ lab_2/french_flag.py
@@ -3,11 +3,14 @@ from config import RED, BLUE, WHITE, END
 def draw_flag() -> None:
     strip_length, flag_length = 7, 5
 
-    flag = []
-    for _ in range(flag_length):
-        flag.append(f'{BLUE}{" " * strip_length}{WHITE}{" " * strip_length}{RED}{" " * strip_length}{END}')
+    # flag = []
+    # for _ in range(flag_length):
+    #     flag.append(f'{BLUE}{" " * strip_length}{WHITE}{" " * strip_length}{RED}{" " * strip_length}{END}')
 
-    print('\n'.join(flag))
+    print('\n'.join(
+        f'{BLUE}{" " * strip_length}{WHITE}{" " * strip_length}{RED}{" " * strip_length}{END}'
+        for _ in range(flag_length)
+    ))
 
 if __name__ == '__main__':
     draw_flag()
