31b19f6998e45f8a088eed39a75c7f4ac9cd2047
8oDolaz <gzagranichnov@mail.ru>
2023-09-18 17:32:51 +0300

    Better method used

--- lab_2/french_flag.py
+++ lab_2/french_flag.py
@@ -3,10 +3,11 @@ from config import RED, BLUE, WHITE, END
 def draw_flag() -> None:
     strip_length, flag_length = 7, 5
 
-    print('\n'.join(
-        f'{BLUE}{" " * strip_length}{WHITE}{" " * strip_length}{RED}{" " * strip_length}{END}'
-        for _ in range(flag_length)
-    ))
+    flag = ''
+    for _ in range(flag_length):
+        flag += f'{BLUE}{" " * strip_length}{WHITE}{" " * strip_length}{RED}{" " * strip_length}{END}\n'
+
+    print(flag)
 
 if __name__ == '__main__':
     draw_flag()
